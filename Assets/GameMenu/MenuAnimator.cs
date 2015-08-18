a68ce6a548e281e80703e6df0eac232b787d8030
Thomas Rouvinez <hadron.anomaly.dev@gmail.com>
2015-08-18 13:08:38 +0200

    Menu clean up & optimizations

--- Assets/GameMenu/MenuAnimator.cs
+++ Assets/GameMenu/MenuAnimator.cs
@@ -9,34 +9,22 @@ public class MenuAnimator : MonoBehaviour {
 	// Variables.
 	// -------------------------------------------------------------------------------------
 
-	private int width;
-	private int height;
-	private int leftMargin;
-	private int topMargin;
-	private int spacing;
-	private int selected;
-	private int creditSelected;
-	private int tempCount;
-	private int tempRandom;
-	private int tmpFontSize;
+	// Optimizations.
+	private float height01, height02, width01, width02, width03; 
+
+	// Layout variables.
+	private int width, height, leftMargin, topMargin, spacing, selected;
+	private int creditSelected, tempCount, tempRandom, tempRandom,browseShip;
+
 	private bool tmpBool;
-	private int browseShip;
 	private string hangarSelected;
+	public float translationSpeed, scaleSpeed, alphaSpeed, alphaSpeedCredits;
 
-	public float translationSpeed;
-	public float scaleSpeed;
-	public float alphaSpeed;
-	public float alphaSpeedCredits;
-
-	public AudioSource click;
-	public AudioSource highscoresRefreshButton;
-	public AudioSource backClick;
+	public AudioSource click, highscoresRefreshButton, backClick;
+	public AudioSource menuMusic, spaceAmbiant;
 	public Camera camera;
-	public GameObject LightBar;
-	public GameObject DarkBar;
+	public GameObject LightBar,DarkBar;
 	public GameObject earth;
-	public AudioSource menuMusic;
-	public AudioSource spaceAmbiant;
 
 	public GUISkin skinMenu;
 	public GUISkin skinHangarButtons;
@@ -76,6 +64,12 @@ public class MenuAnimator : MonoBehaviour {
 	void Start(){
 		width = Screen.width;
      	height = Screen.height;
+		height01 = height * 0.1f;
+		height02 = height * 0.2f;
+		width01 = width * 0.1f;
+		width02 = width * 0.2f;
+		width03 = width * 0.3f;
+
 		leftMargin = (int)(width * 0.04f);
 		topMargin = (int)(height / 3);
 		spacing = (int)((height * 0.1f));
@@ -87,47 +81,44 @@ public class MenuAnimator : MonoBehaviour {
 		highscores = null;
 
 		entries = new LTRect[6];
-		entries[0] = new LTRect(leftMargin, topMargin,(width * 0.3f),(height * 0.1f));				// Race.
-		entries[1] = new LTRect(leftMargin,(topMargin+1*spacing),(width * 0.3f),(height * 0.1f));	// Settings.
-		entries[2] = new LTRect(leftMargin,(topMargin+2*spacing),(width * 0.3f),(height * 0.1f));	// Highscores.
-		entries[3] = new LTRect(leftMargin,(topMargin+3*spacing),(width * 0.3f),(height * 0.1f));	// Hangar.
-		entries[4] = new LTRect(leftMargin,(topMargin+4*spacing),(width * 0.3f),(height * 0.1f));	// Credits.
-		entries[5] = new LTRect(leftMargin,(topMargin+5*spacing),(width * 0.3f),(height * 0.1f));	// Exit.
+		entries[0] = new LTRect(leftMargin, topMargin, width03,(height * 0.1f));							// Race.
+		entries[1] = new LTRect(leftMargin,(topMargin+1*spacing), width03, height01);						// Settings.
+		entries[2] = new LTRect(leftMargin,(topMargin+2*spacing), width03, height01);						// Highscores.
+		entries[3] = new LTRect(leftMargin,(topMargin+3*spacing), width03, height01);						// Hangar.
+		entries[4] = new LTRect(leftMargin,(topMargin+4*spacing), width03, height01);						// Credits.
+		entries[5] = new LTRect(leftMargin,(topMargin+5*spacing), width03, height01);						// Exit.
 
 		menus = new LTRect[12];
-		menus[0] = new LTRect(width, (height * 0.3f),(width * 0.85f), (height * 0.6f));					// Race container.
-		menus[1] = new LTRect(width, (height * 0.3f),(width * 0.85f), (height * 0.6f));					// Exit container.
-		menus[2] = new LTRect((width * 0.1f), (height * 0.2f), (width * 0.9f), (height * 0.8f));		// Credits.
-		menus[6] = new LTRect(width, (height * 0.1f), (width * 0.5f), (height * 0.8f));					// Ship's images.
-		menus[7] = new LTRect((width * 0.1f), height, (width * 0.4f), (height * 0.8f));					// Ship's description.
-		menus[8] = new LTRect((width * 0.17f), height, (width * 0.62f), (height * 0.7f));				// Highscores container.
-		menus[9] = new LTRect(width, (height * 0.3f),(width * 0.85f), (height * 0.6f));					// Settings container.
-		menus[10] = new LTRect((width * 0.1f), (height * 0.3f), (width * 0.9f), (height * 0.7f));		// Credits title + name container.
-		menus[11] = new LTRect((width * 0.1f), (height * 0.4f), (width * 0.9f), (height * 0.6f));		// Credits attributions container.
+		menus[0] = new LTRect(width, (height * 0.3f),(width * 0.85f), (height * 0.6f));						// Race container.
+		menus[1] = new LTRect(width, (height * 0.3f),(width * 0.85f), (height * 0.6f));						// Exit container.
+		menus[2] = new LTRect(width01, height02, (width * 0.9f), (height * 0.8f));							// Credits.
+		menus[6] = new LTRect(width, (height * 0.1f), (width * 0.5f), (height * 0.8f));						// Ship's images.
+		menus[7] = new LTRect(width01, height, (width * 0.4f), (height * 0.8f));							// Ship's description.
+		menus[8] = new LTRect((width * 0.17f), height, (width * 0.62f), (height * 0.7f));					// Highscores container.
+		menus[9] = new LTRect(width, (height * 0.3f),(width * 0.85f), (height * 0.6f));						// Settings container.
+		menus[10] = new LTRect(width01, (height * 0.3f), (width * 0.9f), (height * 0.7f));					// Credits title + name container.
+		menus[11] = new LTRect(width01, (height * 0.4f), (width * 0.9f), (height * 0.6f));					// Credits attributions container.
 
 		buttons = new LTRect[11];
-		buttons[0] = new LTRect(-(width * 0.6f), (height * 0.9f),(width * 0.6f),(height * 0.1f));			// Start race button.
-		buttons[1] = new LTRect(width, (height * 0.675f),(width * 0.6f),(height * 0.1f));					// Confirm exit button.
-		buttons[2] = new LTRect((width * 0.5f),-(height * 0.2f),(width * 0.125f),(height * 0.2f));			// Arnaud credits button.
-		buttons[3] = new LTRect((width * 0.625f),-(height * 0.2f),(width * 0.125f),(height * 0.2f));		// Thomas credits button.
-		buttons[4] = new LTRect((width * 0.75f),-(height * 0.2f),(width * 0.125f),(height * 0.2f));			// Didier credits button.
-		buttons[5] = new LTRect((width * 0.875f),-(height * 0.2f),(width * 0.125f),(height * 0.2f));		// Leonard credits button.
-		buttons[6] = new LTRect(width + (width * 0.1f), (height * 0.25f), (width * 0.1f), (height * 0.7f));	// Highscore refresh button.
-		buttons[7] = new LTRect((width * 0.5f), -(height * 0.15f), (width * 0.5f), (height * 0.1f));		// Ship selection next button.
-		buttons[8] = new LTRect((width * 0.5f), height + (height * 0.15f), (width * 0.5f), (height * 0.1f));// Ship select button.
-		buttons[9] = new LTRect(width, (height * 0.2f), (width * 0.42f), (height * 0.1f));					// Normal game mode.
-		buttons[10] = new LTRect(width, (height * 0.2f), (width * 0.42f), (height * 0.1f));					// Hardcore game mode.
+		buttons[0] = new LTRect(-(width * 0.6f), (height * 0.9f),(width * 0.6f),height01);					// Start race button.
+		buttons[1] = new LTRect(width, (height * 0.675f),(width * 0.6f),height01);							// Confirm exit button.
+		buttons[6] = new LTRect(width + width01, (height * 0.25f), width01, (height * 0.7f));				// Highscore refresh button.
+		buttons[7] = new LTRect((width * 0.5f), -(height * 0.15f), (width * 0.5f), height01);				// Ship selection next button.
+		buttons[8] = new LTRect((width * 0.5f), height + (height * 0.15f), (width * 0.5f), height01);		// Ship select button.
+		buttons[9] = new LTRect(width, height02, (width * 0.42f), height01);								// Normal game mode.
+		buttons[10] = new LTRect(width, height02, (width * 0.42f), height01);								// Hardcore game mode.
 
-		backMenu = new LTRect(-(width * 0.1f), (height * 0.2f),(width * 0.1f),(height * 0.8f));
+		backMenu = new LTRect(-width01, height02, width01,(height * 0.8f));
 		backMenu.alpha = 0f;
 
 		exitMessages = new string[]{"[Yes] I pretend I have work to do",
 									"[Yes] I'm a sissie", 
 									"[Yes] This is game is too hard for me",
-									"[Yes] I give up easily..."};
+									"[Yes] I give up easily...",
+									"[Yes] I have a weak will..."};
 		
 		top = new Vector2(0f, 0f);
-		topSize = new Vector2((width * 0.5f), (height * 0.2f));
+		topSize = new Vector2((width * 0.5f), height02);
 		creditSelected = 2;
 
 		resetBars();
@@ -207,30 +198,16 @@ public class MenuAnimator : MonoBehaviour {
 			select(entries[3]);
 
 			LeanTween.move(buttons[7], new Vector2(buttons[7].rect.x, 0f), translationSpeed).setEase(LeanTweenType.easeInOutBack);
-			LeanTween.move(buttons[8], new Vector2(buttons[8].rect.x, height - (height * 0.1f)), translationSpeed).setEase(LeanTweenType.easeInOutBack);
+			LeanTween.move(buttons[8], new Vector2(buttons[8].rect.x, height - height01), translationSpeed).setEase(LeanTweenType.easeInOutBack);
 			LeanTween.move(menus[6], new Vector2((width * 0.5f), menus[6].rect.y), translationSpeed * 1.5f);
-			LeanTween.move(menus[7], new Vector2(menus[7].rect.x, (height * 0.2f)), translationSpeed * 2f);
+			LeanTween.move(menus[7], new Vector2(menus[7].rect.x, height02), translationSpeed * 2f);
 		}
 
 		if(GUI.Button(entries[4].rect, "<size=" + (entries[4].rect.width * 0.148) + ">CREDITS</size>")){
 			selected = 4; tempCount++;
 			select(entries[4]);
 
-			LeanTween.move(buttons[2], new Vector2(buttons[2].rect.x, 0), 0.15f);
-			LeanTween.move(buttons[3], new Vector2(buttons[3].rect.x, 0), 0.2f);
-			LeanTween.move(buttons[4], new Vector2(buttons[4].rect.x, 0), 0.25f);
-			LeanTween.move(buttons[5], new Vector2(buttons[5].rect.x, 0), 0.3f);
-
-			LeanTween.move(menus[10], new Vector2((width * 0.1f), menus[10].rect.y), translationSpeed);
-
-			for(int i = 2 ; i < 6 ; i++){
-				if(i != creditSelected){
-					LeanTween.alpha(menus[i], 0f, 0.1f);
-				}
-				else{
-					LeanTween.alpha(menus[i], 1f, alphaSpeedCredits).setEase(LeanTweenType.easeInOutBack);
-				}
-			}
+			LeanTween.move(menus[10], new Vector2(width01, menus[10].rect.y), translationSpeed);
 		}
 
 		if(GUI.Button(entries[5].rect, "<size=" + (entries[5].rect.width * 0.15) + ">EXIT</size>")){
@@ -292,11 +269,11 @@ public class MenuAnimator : MonoBehaviour {
 
 			tmpFontSize = (int) ((width * 0.5f) * 0.05);
 
-			GUI.Label(new Rect ((width * 0.2f), (height * 0.4f), (width * 0.7f), (height * 0.1f)), "<size=" + tmpFontSize + ">" + "ENABLE IN-GAME MUSIC</size>");
-			GUI.Label(new Rect ((width * 0.2f), (height * 0.5f), (width * 0.7f), (height * 0.1f)), "<size=" + tmpFontSize + ">" + "ENABLE MENU MUSIC</size>");
+			GUI.Label(new Rect (width02, (height * 0.4f), (width * 0.7f), height01), "<size=" + tmpFontSize + ">" + "ENABLE IN-GAME MUSIC</size>");
+			GUI.Label(new Rect (width02, (height * 0.5f), (width * 0.7f), height01), "<size=" + tmpFontSize + ">" + "ENABLE MENU MUSIC</size>");
 
-			GameConfiguration.Instance.gameMusicOn = GUI.Toggle (new Rect ((width * 0.8f), (height * 0.4f), (height * 0.1f) ,(height * 0.1f)), GameConfiguration.Instance.gameMusicOn, "");
-			GameConfiguration.Instance.menuMusicOn = GUI.Toggle (new Rect ((width * 0.8f), (height * 0.5f), (height * 0.1f) ,(height * 0.1f)), GameConfiguration.Instance.menuMusicOn, "");
+			          GameConfiguration.Instance.gameMusicOn = GUI.Toggle (new Rect ((width * 0.8f), (height * 0.4f), height01 ,height01), GameConfiguration.Instance.gameMusicOn, "");
+			          GameConfiguration.Instance.menuMusicOn = GUI.Toggle (new Rect ((width * 0.8f), (height * 0.5f), height01 ,height01), GameConfiguration.Instance.menuMusicOn, "");
 
 			// Save settings.
 			PlayerPrefs.SetInt("gameMusicOn", GameConfiguration.Instance.gameMusicOn == true ? 1:0);
@@ -324,7 +301,7 @@ public class MenuAnimator : MonoBehaviour {
 						if(long.Parse(hs.score) > (long)PlayerPrefs.GetFloat("highscore", 0f) && count < 9){
 							GUI.Box(new Rect((width * 0.18f),padd,(width * 0.05f), listHeight), "<size=" + tmpFontSize + ">" + hs.position + "</size>");
 							GUI.Box(new Rect((width * 0.25f),padd,(width * 0.4f), listHeight), "<size=" + tmpFontSize + ">" + hs.name + "</size>");
-							GUI.Box(new Rect((width * 0.6f),padd,(width * 0.2f), listHeight), "<size=" + tmpFontSize + ">" + hs.score + "</size>");
+							GUI.Box(new Rect((width * 0.6f),padd, width02, listHeight), "<size=" + tmpFontSize + ">" + hs.score + "</size>");
 							padd += listHeight;
 							count ++;
 						}
@@ -332,7 +309,7 @@ public class MenuAnimator : MonoBehaviour {
 							if(isIn == false){
 								GUI.Box(new Rect((width * 0.18f),padd,(width * 0.05f), listHeight), "<size=" + tmpFontSize + ">>" + ">" + "</size>");
 								GUI.Box(new Rect((width * 0.25f),padd,(width * 0.4f), listHeight), "<size=" + tmpFontSize + ">" + "YOUR BEST" + "</size>");
-								GUI.Box(new Rect((width * 0.6f),padd,(width * 0.2f), listHeight), "<size=" + tmpFontSize + ">" + (long)PlayerPrefs.GetFloat("highscore", 0f) + "</size>");
+								GUI.Box(new Rect((width * 0.6f),padd, width02, listHeight), "<size=" + tmpFontSize + ">" + (long)PlayerPrefs.GetFloat("highscore", 0f) + "</size>");
 								padd += listHeight;
 								count ++;
 								isIn = true;
@@ -341,7 +318,7 @@ public class MenuAnimator : MonoBehaviour {
 							if(count < 10){
 								GUI.Box(new Rect((width * 0.18f),padd,(width * 0.05f), listHeight), "<size=" + tmpFontSize + ">" + hs.position + "</size>");
 								GUI.Box(new Rect((width * 0.25f),padd,(width * 0.4f), listHeight), "<size=" + tmpFontSize + ">" + hs.name + "</size>");
-								GUI.Box(new Rect((width * 0.6f),padd,(width * 0.2f), listHeight), "<size=" + tmpFontSize + ">" + hs.score + "</size>");
+								GUI.Box(new Rect((width * 0.6f),padd, width02, listHeight), "<size=" + tmpFontSize + ">" + hs.score + "</size>");
 								padd += listHeight;
 								count ++;
 							}
@@ -354,7 +331,7 @@ public class MenuAnimator : MonoBehaviour {
 			else{
 				// If not loaded yet, display a waiting message.
 				GUI.skin = skinHighscoresButton;
-				GUI.Box(new Rect((width * 0.35f),(height * 0.45f),(width * 0.3f),(height * 0.1f)), "");	// Loading label.
+				GUI.Box(new Rect((width * 0.35f),(height * 0.45f), width03,(height * 0.1f)), "");	// Loading label.
 			}
 
 			GUI.skin = skinHighscoresButton;
@@ -444,7 +421,7 @@ public class MenuAnimator : MonoBehaviour {
 		menuTranslate(selected);
 
 		if(tempCount < 1){
-			LeanTween.move(backMenu, new Vector2(backMenu.rect.x + (width * 0.1f), backMenu.rect.y), translationSpeed);
+					LeanTween.move(backMenu, new Vector2(backMenu.rect.x + width01, backMenu.rect.y), translationSpeed);
 			click.GetComponent<AudioSource>().Play();
 		}
 	}
@@ -453,14 +430,14 @@ public class MenuAnimator : MonoBehaviour {
 		for(int i = 0; i < entries.Length; i++){
 			if(i != selected){
 				LeanTween.alpha(entries[i], 0f, alphaSpeed);
-				LeanTween.move(entries[i], new Vector2(entries[i].rect.x - width + (width * 0.1f), entries[i].rect.y), translationSpeed);
+						LeanTween.move(entries[i], new Vector2(entries[i].rect.x - width + width01, entries[i].rect.y), translationSpeed);
 			}
 		}
 	}
 
 	private void reset(){
 		// Hide the back bar.
-		LeanTween.move(backMenu, new Vector2(backMenu.rect.x -(width * 0.1f), backMenu.rect.y), translationSpeed);
+		LeanTween.move(backMenu, new Vector2(backMenu.rect.x -width01, backMenu.rect.y), translationSpeed);
 
 		// Restore all entries.
 		for(int i = 0 ; i < entries.Length; i++){
@@ -471,7 +448,7 @@ public class MenuAnimator : MonoBehaviour {
 		}
 
 		LeanTween.move(entries[selected], new Vector2(leftMargin, topMargin + selected * spacing), translationSpeed);
-		LeanTween.scale(entries[selected], new Vector2((width * 0.3f),(height * 0.1f)), scaleSpeed);
+				LeanTween.scale(entries[selected], new Vector2(width03, height01), scaleSpeed);
 
 		// Reset bars.
 		resetBars();
@@ -481,8 +458,8 @@ public class MenuAnimator : MonoBehaviour {
 		case 0:
 			LeanTween.move(menus[0], new Vector2(width, menus[0].rect.y), translationSpeed);
 			LeanTween.move(buttons[0], new Vector2(-(width * 0.6f), buttons[0].rect.y), translationSpeed);
-			LeanTween.move(buttons[9], new Vector2(width, (height * 0.2f)), translationSpeed);
-			LeanTween.move(buttons[10], new Vector2(width, (height * 0.2f)), translationSpeed);
+			LeanTween.move(buttons[9], new Vector2(width, height02), translationSpeed);
+			LeanTween.move(buttons[10], new Vector2(width, height02), translationSpeed);
         	break;
 
 		case 1:
@@ -495,17 +472,13 @@ public class MenuAnimator : MonoBehaviour {
 			break;
 
 		case 3:
-			LeanTween.move(buttons[7], new Vector2(buttons[7].rect.x,-(height * 0.1f)), translationSpeed);
-			LeanTween.move(buttons[8], new Vector2(buttons[8].rect.x, height + (height * 0.1f)), translationSpeed);
+					LeanTween.move(buttons[7], new Vector2(buttons[7].rect.x,-height01), translationSpeed);
+					LeanTween.move(buttons[8], new Vector2(buttons[8].rect.x, height + height01), translationSpeed);
 			LeanTween.move(menus[6], new Vector2(width, menus[6].rect.y), translationSpeed);
 			LeanTween.move(menus[7], new Vector2(menus[7].rect.x, height), translationSpeed);
 			break;
 
 		case 4:
-			LeanTween.move(buttons[2], new Vector2(buttons[2].rect.x, -(width * 0.2f)), translationSpeed);
-			LeanTween.move(buttons[3], new Vector2(buttons[3].rect.x, -(width * 0.2f)), translationSpeed);
-			LeanTween.move(buttons[4], new Vector2(buttons[4].rect.x, -(width * 0.2f)), translationSpeed);
-			LeanTween.move(buttons[5], new Vector2(buttons[5].rect.x, -(width * 0.2f)), translationSpeed);
 			break;
 
 		case 5:
